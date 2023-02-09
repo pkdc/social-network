fcc01e5d227a1170c07c678f3f9a4386f4d52330
pkdc <pkdchoy@gmail.com>
2023-02-09 17:44:53 +0000

    styling changes

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -6,6 +6,8 @@ import SubmitButton from "./UI/SubmitButton";
 import styles from "./LoginForm.module.css";
 
 const LoginForm = () => {
+    const loginURL = "http://localhost:8080/login/";
+    
     const [enteredEmail, setEnteredEmail] = useState("");
     const [enteredPw, setEnteredPw] = useState("");
 
@@ -20,14 +22,25 @@ const LoginForm = () => {
         
     const submitHandler = (e) => {
         e.preventDefault();
-        const lgoinPayloadObj = {
+        const loginPayloadObj = {
             email: enteredEmail,
             pw: enteredPw
         };
-        console.log(lgoinPayloadObj);
+        console.log(loginPayloadObj);
 
-        setEnteredEmail("");
-        setEnteredPw("");
+        const reqOptions = {
+            method: "POST",
+            body: JSON.stringify(loginPayloadObj)
+        };
+        fetch(loginURL, reqOptions)
+        .then(resp => {
+            const respObj = resp.json();
+            console.log(respObj);
+            
+        }
+        )
+        // setEnteredEmail("");
+        // setEnteredPw("");
     };
 
     return (
@@ -37,6 +50,7 @@ const LoginForm = () => {
             <FormLabel htmlFor="password">Password</FormLabel>
             <FormInput type="password" name="password" id="password" placeholder="Password" value={enteredPw} onChange={pwChangeHandler}/>
             <SubmitButton type="submit">Login</SubmitButton>
+            <p>Don't have an account? Register one</p>
         </Form>
     )
 };
--- frontend/src/components/UI/Form.js
+++ frontend/src/components/UI/Form.js
@@ -4,8 +4,8 @@ import styles from "./Form.module.css";
 const Form = (props) => {
     return (
         <>
-            {!props.className && <form className={`${styles["form"]}`}>{props.children}</form>}
-            {props.className && <form className={`${styles["form"]} ${styles[props.className]}`}>{props.children}</form>}
+            {!props.className && <form className={`${styles["form"]}`} onSubmit={props.onSubmit}>{props.children}</form>}
+            {props.className && <form className={`${styles["form"]} ${styles[props.className]}`} onSubmit={props.onSubmit}>{props.children}</form>}
         </>
     )
 
