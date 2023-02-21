c9927906acf5c0f7408076c681cf70930564b5a1
Anthony D'Souza <65923760+anthony-dsouza@users.noreply.github.com>
2023-02-21 19:19:02 +0000

    Add files via upload

--- backend/cmd/web/main.go
+++ backend/cmd/web/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"backend"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os/exec"
 
@@ -14,56 +16,84 @@ import (
 )
 
 func main() {
-
-	
 	m, err := migrate.New(
-        "file://../../pkg/db/migration/sqlite",
-        "sqlite3://../../pkg/db/database.db")
-    m.Up()
+		"file://../../pkg/db/migration/sqlite",
+		"sqlite3://../../pkg/db/database.db")
+	m.Up()
 
 	if err != nil {
 		fmt.Print(err.Error())
 	}
-
 	// connect to database
 
-	// var db *sql.DB
-	
-	// db, _ = sql.Open("sqlite3", "../../pkg/db/database.db")
+	var db *sql.DB
+
+	db, _ = sql.Open("sqlite3", "../../pkg/db/database.db")
 
 	// fetch api for mock data
-	
+
 	var res *http.Response
 
 	res, _ = http.Get("https://63f35a0e864fb1d60014de90.mockapi.io/users")
-	
+
 	resData, _ := ioutil.ReadAll(res.Body)
 
-
-	
 	type User struct {
 		FirstName string `json:"firstName"`
-		LastName string `json:"lastName"`
-		NickName string `json:"nickName"`
-		Email string `json:"email"`
-		Password string `json:"password"`
-		Dob string `json:"dob"`
-		Image string `json:"image"`
-		About string `json:"about"`
-		Public bool `json:"public"`
+		LastName  string `json:"lastName"`
+		NickName  string `json:"nickName"`
+		Email     string `json:"email"`
+		Password  string `json:"password"`
+		Dob       string `json:"dob"`
+		Image     string `json:"image"`
+		About     string `json:"about"`
+		Public    bool   `json:"public"`
 	}
 
+	type Users struct {
+		ID        string `json:"id"`
+		FirstName string `json:"firstName"`
+		LastName  string `json:"lastName"`
+		NickName  string `json:"nickName"`
+		Email     string `json:"email"`
+		Password  string `json:"password"`
+		Dob       string `json:"dob"`
+		Image     string `json:"image"`
+		About     string `json:"about"`
+		Public    bool   `json:"public"`
+	}
 
-/// receiving information
+	var curUser Users
+
+	/// receiving information
 
 	var responseObject []User
 
 	json.Unmarshal(resData, &responseObject)
 
-	fmt.Println(responseObject)
-
 	for _, user := range responseObject {
-		fmt.Println(user.FirstName)
+		// insert user data
+
+		stmt, err := db.Prepare("INSERT INTO user(firstName, lastName, nickName, email, password_, dob, image_, about, public) VALUES(?,?,?,?,?,?,?,?,?);")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer stmt.Close()
+
+		stmt.Exec(user.FirstName, user.LastName, user.NickName, user.Email, user.Password, user.Dob, user.Image, user.About, 0)
+	}
+
+	rows, err := db.Query(`SELECT id, firstName, lastName, nickName, email, password_, dob, image_, about, public FROM user`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		// rows3.Scan(&id, &curNickname, &curAge, &curGender, &curFirstName, &curLastName, &curEmail, &pw, &curLoggedIn)
+		rows.Scan(&curUser.ID, &curUser.FirstName, &curUser.LastName, &curUser.NickName, &curUser.Email, &curUser.Password, &curUser.Dob, &curUser.Image, &curUser.About, &curUser.Public)
+		fmt.Println(curUser)
 	}
 
 	exec.Command("xdg-open", "https://localhost/").Start()
