4f1d838bb28fce4bd66c04bab2c867a66837bb96
pkdc <pkdchoy@gmail.com>
2023-02-22 13:58:32 +0000

    seperate CreatePost and CreateGroupPost, since they will have different functions like CreatePost can choose privacy etc

--- frontend/src/components/pages/PostsPage.js
+++ frontend/src/components/pages/PostsPage.js
@@ -1,7 +1,40 @@
 import React from "react";
+import FormLabel from "../UI/FormLabel";
+import FormInput from "../UI/FormInput";
+import FormTextarea from "../UI/FormTextarea";
+import styles from "./PostsPage.module.css";
+import CreatePost from "../posts/CreatePost";
+import AllPosts from "../posts/AllPosts";
 
 const PostsPage = () => {
-    return <p>Posts</p>
+    const DATA = [
+        {
+            id: 1,
+            user: 'username',
+            content: 'this is the post content',
+            date: 'date'
+    },
+    {
+        id: 2,
+        user: 'username2',
+        content: 'this is the post content2',
+        date: 'date2'
+    }
+    ]
+
+    return (
+        <>
+        <h1 className={styles["title"]}>Create New Post</h1>
+        <div className={styles["container"]}>
+            <div className={styles["create-post"]}>
+                <CreatePost />
+            </div>
+            <div className={styles["all-posts"]}>
+                <AllPosts posts={DATA}/>
+            </div>
+        </div>
+        </>
+    )
 };
 
 export default PostsPage;
