12878dc713ada4177822a0156f1320cfc744b311
anthony <anthonydsouza58@gmail.com>
2023-03-08 14:12:52 +0000

    group event queries

--- backend/pkg/db/queries/groupPostCommentQuery.sql
+++ backend/pkg/db/queries/groupPostCommentQuery.sql
@@ -3,7 +3,7 @@ SELECT * FROM group_post_comment
 WHERE group_post_id = ?
 ORDER BY created_at;
 
--- name: CreateGroupPost :one
+-- name: CreateGroupPostComment :one
 INSERT INTO group_post_comment (
   author, group_post_id, message_, created_at
 ) VALUES (
@@ -11,6 +11,6 @@ INSERT INTO group_post_comment (
 )
 RETURNING *;
 
--- name: DeleteGroupMessage :exec
+-- name: DeleteGroupPostComment :exec
 DELETE FROM group_post_comment
 WHERE group_post_id = ? AND author = ? AND id = ?;
