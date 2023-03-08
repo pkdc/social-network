aad89db2ae27543eedc8e3684803a540dfc231e3
anthony <anthonydsouza58@gmail.com>
2023-03-08 12:42:24 +0000

    post comment queries

---
+++ backend/pkg/db/queries/postComment.sql
@@ -0,0 +1,16 @@
+-- name: GetPostComments :many
+SELECT * FROM post_comment
+WHERE post_id = ?
+ORDER BY created_at;
+
+-- name: CreatePostComment :one
+INSERT INTO post_comment (
+  user_id, post_id, created_at, message_
+) VALUES (
+  ?, ?, ?, ?
+)
+RETURNING *;
+
+-- name: DeletePostComment :exec
+DELETE FROM post_comment
+WHERE user_id = ? AND post_id = ?;
