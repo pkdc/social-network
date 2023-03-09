418cfbd12a0c3ad5e3d6cea84488b3fd48b1285f
mjc2001 <mjchapman2001@gmail.com>
2023-03-09 14:58:49 +0000

    Added endpoints to the server

--- backend/cmd/web/main.go
+++ backend/cmd/web/main.go
@@ -13,12 +13,23 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.Handle("/", backend.Homehandler())
+	mux.Handle("/session", backend.SessionHandler())
 	mux.Handle("/login", backend.Loginhandler())
 	mux.Handle("/logout", backend.Logouthandler())
 	mux.Handle("/reg", backend.Reghandler())
 	mux.Handle("/user", backend.Userhandler())
+	mux.Handle("/user-follower", backend.UserFollowerHandler())
+	mux.Handle("/user-message", backend.UserMessageHandler())
 	mux.Handle("/post", backend.Posthandler())
+	mux.Handle("/post-comment", backend.PostCommentHandler())
 	mux.Handle("/group", backend.Grouphandler())
+	mux.Handle("/group-member", backend.GroupMemberHandler())
+	mux.Handle("/group-request", backend.GroupRequestHandler())
+	mux.Handle("/group-post", backend.GroupPostHandler())
+	mux.Handle("/group-post-comment", backend.GroupPostCommentHandler())
+	mux.Handle("/group-event", backend.GroupEventHandler())
+	mux.Handle("/group-event-member", backend.GroupEventMemberHandler())
+	mux.Handle("/group-message", backend.GroupMessageHandler())
 
 	fmt.Println("Starting server at port 8080")
 
