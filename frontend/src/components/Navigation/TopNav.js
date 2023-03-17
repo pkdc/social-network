96a90b94bbd5780255570043aeb5fadefc8e4e2c
pkdc <pkdchoy@gmail.com>
2023-03-17 16:16:22 +0000

    apply styles when link is active

--- frontend/src/components/Navigation/TopNav.js
+++ frontend/src/components/Navigation/TopNav.js
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import LogoutButton from "../UI/LogoutButton";
 import NotificationBtn from "../UI/NotificationBtn";
 import styles from "./TopNav.module.css";
@@ -6,6 +6,13 @@ import logout from "../assets/logout.svg";
 import profile from "../assets/profileSmall.svg";
 
 const TopMenu = (props) => {
+    const defaultImagePath = "default_avatar.jpg";
+    const userId = +localStorage.getItem("user_id");
+    const first = localStorage.getItem("fname");
+    const last = localStorage.getItem("lname");
+    const nickname = localStorage.getItem("nname");
+    const avatar = localStorage.getItem("avatar");
+    
     const navigate = useNavigate();
     const onClickingLogout = () => {
         props.onLogout();
@@ -17,9 +24,9 @@ const TopMenu = (props) => {
             <div className={styles["top-nav"]}>
                 <Link to={"/"} className={styles.logo}>notFacebook</Link>
                 <div className={styles.menu}>
-                    <Link className={styles.lnk} to="/">Home</Link>
-                    <Link className={styles.lnk} to="/group">Groups</Link>
-                    <Link className={styles.lnk} to="/messanger">Messenger</Link>
+                    <NavLink className={({isActive}) => isActive ? styles["active"] : undefined} to="/">Home</NavLink>
+                    <NavLink className={({isActive}) => isActive ? styles["active"] : undefined} to="/group">Groups</NavLink>
+                    <NavLink className={({isActive}) => isActive ? styles["active"] : undefined} to="/messanger">Messenger</NavLink>
                     <div className={styles.profile}>
                     {/* <img src={profile} alt=""/> */}
                     <Link className={styles.profile} to="/profile">
