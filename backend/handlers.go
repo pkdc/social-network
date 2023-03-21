9850e903018b0144f672422528880dd4970ba6ec
mjc2001 <mjchapman2001@gmail.com>
2023-03-21 09:45:40 +0000

    Added allow methods, headers and credentials to cors

--- backend/handlers.go
+++ backend/handlers.go
@@ -23,6 +23,9 @@ func WriteHttpHeader(jsonResp []byte, w http.ResponseWriter) {
 
 func EnableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Authorization")
+	(*w).Header().Set("Access-Control-Allow-Credentials", "true")
 }
 
 func Homehandler() http.HandlerFunc {
