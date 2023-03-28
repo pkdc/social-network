e3b12917c4f912160378d702e586adbec9ee29cf
anthony <anthonydsouza58@gmail.com>
2023-03-28 21:59:36 +0100

    get request

--- backend/handlers.go
+++ backend/handlers.go
@@ -1971,6 +1971,12 @@ func GroupEventMemberHandler() http.HandlerFunc {
 			return
 		}
 
+		eId, err := strconv.Atoi(eventId)
+
+		if err != nil {
+			fmt.Println("Unable to convert event ID")
+		}
+
 		switch r.Method {
 		case http.MethodGet:
 			// Declares the payload struct
@@ -1978,7 +1984,28 @@ func GroupEventMemberHandler() http.HandlerFunc {
 
 			// ### CONNECT TO DATABASE ###
 
+			db := db.DbConnect()
+
+			query := crud.New(db)
+
 			// ### GET ALL GROUP EVENT MEMBERS ###
+
+			members, err := query.GetGroupEventMembers(context.Background(), int64(eId))
+
+			if err != nil {
+				fmt.Println("Unable to get all members")
+			}
+
+			for _, member := range members {
+				var newMember GroupEventMemberStruct
+
+				newMember.Id = int(member.ID)
+				newMember.Status = int(member.Status)
+				newMember.UserId = int(member.UserID)
+				newMember.EventId = int(member.EventID)
+
+				Resp.Data = append(Resp.Data, newMember)
+			}
 
 			// Marshals the response struct to a json object
 			jsonResp, err := json.Marshal(Resp)
