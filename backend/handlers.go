4faf7897e9169b00df5d06b11fbe10cf432c9a41
anthony <anthonydsouza58@gmail.com>
2023-03-28 22:15:24 +0100

    post request

--- backend/handlers.go
+++ backend/handlers.go
@@ -2029,7 +2029,49 @@ func GroupEventMemberHandler() http.HandlerFunc {
 
 			// ### CONNECT TO DATABASE ###
 
+			db := db.DbConnect()
+
+			query := crud.New(db)
+
 			// ### ADD/UPDATE GROUP EVENT MEMBER TO DATABASE ###
+
+			exists, err := query.GetGroupEventMember(context.Background(), crud.GetGroupEventMemberParams{
+				EventID: int64(groupPost.EventId),
+				UserID:  int64(groupPost.UserId),
+			})
+
+			if err != nil {
+				Resp.Success = false
+				fmt.Println("Unable to get event member")
+			}
+
+			if exists > 0 {
+				// update
+				_, err = query.UpdateGroupEventMember(context.Background(), crud.UpdateGroupEventMemberParams{
+					Status:  int64(groupPost.Status),
+					EventID: int64(groupPost.EventId),
+					UserID:  int64(groupPost.UserId),
+				})
+
+				if err != nil {
+					Resp.Success = false
+					fmt.Println("Unable to update event member")
+				}
+
+			} else {
+				// add
+				_, err = query.CreateGroupEventMember(context.Background(), crud.CreateGroupEventMemberParams{
+					Status:  int64(groupPost.Status),
+					EventID: int64(groupPost.EventId),
+					UserID:  int64(groupPost.UserId),
+				})
+
+				if err != nil {
+					Resp.Success = false
+					fmt.Println("Unable to add event member")
+				}
+
+			}
 
 			// Marshals the response struct to a json object
 			jsonResp, err := json.Marshal(Resp)
