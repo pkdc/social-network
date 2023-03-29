828bc06d48abe97a7fc68d92dbf6aec471141c05
pkdc <pkdchoy@gmail.com>
2023-03-29 15:57:26 +0000

    show chat sidebar done

--- frontend/src/components/Navigation/ChatSidebar.js
+++ frontend/src/components/Navigation/ChatSidebar.js
@@ -4,26 +4,28 @@ import { useState } from "react";
 
 const ChatSidebar = () => {
     const [hovered, setHovered] = useState(false);
+    const [showChat, setShowChat] = useState(false);
 
-    const showChatSidebarHandler = () => {};
+    const clickHandler = () => !showChat ? setShowChat(true) : setShowChat(false);
 
     // const sidebarHoveredHandler = () => {};
     return (
         <>
         <div 
-            className={`${styles["sidebar"]} ${hovered ? styles["hovered"] : ""}`} 
+            className={`${styles["sidebar"]} ${hovered ? styles["hovered"] : ""} ${showChat ? styles["show-chat"] : ""}`}
             onMouseEnter={() => setHovered(true)} 
             onMouseLeave={() => setHovered(false)}
+            onClick={clickHandler}
             >
         </div>
         <div>
         {/* <div className={classes.right}> */}
         <button 
-            className={`${styles["show-sidebar-btn"]} ${hovered ? styles["hovered"] : ""}`} 
-            onClick={showChatSidebarHandler} 
+            className={`${styles["show-sidebar-btn"]} ${hovered ? styles["hovered"] : ""} ${showChat ? styles["show-chat"] : ""}`} 
             onMouseEnter={() => setHovered(true)}
             onMouseLeave={() => setHovered(false)}
-            >&lt;&lt;</button>
+            onClick={clickHandler}
+            >{showChat ? ">" : "<"}</button>
         </div>
         
         </>
