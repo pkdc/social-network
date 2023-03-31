3ecbcdaec591032b08d200ed2913a80cb4eb52a7
pkdc <pkdchoy@gmail.com>
2023-03-31 13:46:42 +0000

    filter out self in user list

--- frontend/src/components/chat/UserChat/AllUserChatItems.js
+++ frontend/src/components/chat/UserChat/AllUserChatItems.js
@@ -4,19 +4,23 @@ const AllUserChatItems = (props) => {
 
     // console.log("user chat followers in AllUserChatItems", props.followersList);
     // console.log("isArray", Array.isArray(props.followersList));
+    const curUserId = +localStorage.getItem("user_id");
     return (
         <div>
             {props.followersList.map((follower) => {
                 // console.log("each follower", follower);
-
-                return <UserChatItem 
+                console.log("curUserId: ", curUserId);
+                console.log("follower.id", follower.id);
+                {if (curUserId !== follower.id) {
+                    return <UserChatItem 
                     key={follower.id}
                     id={follower.id}
                     avatar={follower.avatar}
                     fname={follower.fname}
                     lname={follower.lname}
                     nname={follower.nname}
-                />
+                />}
+                }
             })}
         </div>
     );
