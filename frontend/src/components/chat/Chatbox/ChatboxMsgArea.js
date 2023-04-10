e50145493c43fab8f007770fb8bfd65ee0f3715a
pkdc <pkdchoy@gmail.com>
2023-04-10 16:10:41 +0000

    style wip2

--- frontend/src/components/chat/Chatbox/ChatboxMsgArea.js
+++ frontend/src/components/chat/Chatbox/ChatboxMsgArea.js
@@ -4,21 +4,11 @@ import AllNewMsgItems from "./AllNewMsgItems";
 import styles from "./ChatboxMsgArea.module.css";
 
 const ChatboxMsgArea = (props) => {
-
+    console.log("scrollY in ChatboxMsgArea", );
     return (
         <Card className={styles["msg-area"]} style={{height: `${window.innerHeight-316}px`}}>
             <AllOldMsgItems oldMsgItems={props.oldMsgItems}/>
             <AllNewMsgItems newMsgItems={props.newMsgItems}/>
-            {/* {props.msgItems.map((msg, m) => (
-                <div>
-                    {msg.message}
-                </div>
-            ))}
-            {props.newMsgs.map((msg, m) => (
-                <div>
-                    {msg.message}
-                </div>
-            ))} */}
         </Card>
     );
 };
