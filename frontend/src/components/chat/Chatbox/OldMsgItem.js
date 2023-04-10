49da74aa98fc2c620cdb44522554d7b9cbc88033
pkdc <pkdchoy@gmail.com>
2023-04-10 15:29:12 +0000

    style

--- frontend/src/components/chat/Chatbox/OldMsgItem.js
+++ frontend/src/components/chat/Chatbox/OldMsgItem.js
@@ -4,8 +4,10 @@ const OldMsgItem = (props) => {
     const selfId = +localStorage.getItem("user_id");
     console.log("selfId old msg", selfId);
     return (
-        <div className={`${props.sourceid === selfId ? styles["self-msg"] : styles["frd-msg"]}`}>    
-        {props.msg}
+        <div className={`${props.sourceid === selfId ? styles["self-msg"] : styles["frd-msg"]}`}>
+            <div className={`${props.sourceid === selfId ? styles["chat-bubble-self"] : styles["chat-bubble-frd"]}`}>
+            {props.msg}
+            </div>
         </div>
     );
 };
