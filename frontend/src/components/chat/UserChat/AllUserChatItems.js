60a98d4509870404a627f322d6ee2c19d28d2c95
pkdc <pkdchoy@gmail.com>
2023-05-20 12:43:39 +0000

    fixed bug2

--- frontend/src/components/chat/UserChat/AllUserChatItems.js
+++ frontend/src/components/chat/UserChat/AllUserChatItems.js
@@ -34,8 +34,8 @@ const AllUserChatItems = (props) => {
 
     const usersCtx = useContext(UsersContext);
     console.log("users (chatitems)", usersCtx.users);
-    const followingUids = []; // in case followingCtx.following is empty
-    followingCtx.following && followingCtx.following.map((following) => following.id);
+    let followingUids = []; // in case followingCtx.following is empty
+    if (followingCtx.following) followingUids = followingCtx.following.map((following) => following.id);
     console.log("following id (chatitems)", followingUids);
     // const publicUsers = usersCtx.users.filter((user) => user.public === 1 && !followingCtx.following.includes(user));
     const publicUsers = usersCtx.users.filter((user) => user.public === 1 && !followingUids.includes(user.id));
