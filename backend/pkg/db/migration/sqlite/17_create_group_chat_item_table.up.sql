31d639fe2eea7d587dabf2fc9364b90f2f71ddf7
anthony <anthonydsouza58@gmail.com>
2023-06-12 13:30:46 +0100

    group chat item

---
+++ backend/pkg/db/migration/sqlite/17_create_group_chat_item_table.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE group_chat_item (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  group_id INTEGER NOT NULL,
+  last_msg_at DATETIME NOT NULL,
+  FOREIGN KEY (group_id) REFERENCES group_(id),
+);
