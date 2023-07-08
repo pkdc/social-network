92508ce182bfb8a7c7a6c41143941d001ebd1151
pkdc <pkdchoy@gmail.com>
2023-07-08 11:10:00 +0000

    changes chatdetailtopbar

--- frontend/src/components/chat/GroupChat/GroupChatDetailTopBar.js
+++ frontend/src/components/chat/GroupChat/GroupChatDetailTopBar.js
@@ -1,30 +1,29 @@
 import { useNavigate } from "react-router-dom";
 import Card from "../../UI/Card";
-import useGet from "../../fetch/useGet";
+import { GroupsContext } from "../../store/groups-context";
 import styles from "../UserChat/ChatDetailTopBar.module.css";
+import { useContext } from "react";
 const GroupChatDetailTopBar = (props) => {
 
     const navigate = useNavigate();
     
+    // const { error, isLoaded, data } = useGet(`/group?id=${props.groupId}`)
 
-    const { error, isLoaded, data } = useGet(`/group?id=${props.groupId}`)
+    // if (!isLoaded) return <div>Loading...</div>
+    // if (error) return <div>Error: {error.message}</div>
 
-    if (!isLoaded) return <div>Loading...</div>
-    if (error) return <div>Error: {error.message}</div>
-
+    const grpCtx = useContext(GroupsContext);
 
     function handleClick() {
         const id =  props.groupId
         navigate("/groupprofile", { state: { id } })
-        
     }
 
     return (
         <Card className={styles["container"]}>
             <div className={styles.chatdetails}>
                 <div className={styles.lnk} onClick={handleClick}>
-
-                    {data.data[0].title}
+                    {grpCtx.groups.find(grp => grp.id === props.groupId).title}
                 </div>
             </div>
         </Card>
