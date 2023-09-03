38c9b77617ed57021f2febf535662f29688352cd
pkdc <pkdchoy@gmail.com>
2023-09-03 06:38:25 +0100

    test choose chat done

--- frontend/src/components/chat/ChooseChat.test.js
+++ frontend/src/components/chat/ChooseChat.test.js
@@ -4,16 +4,51 @@ import userEvent from "@testing-library/user-event";
 import ChooseChat from "./ChooseChat";
 
 describe("Choose Chat", () => {
-    test("render choose user chat btn", () => {
-        // Arange
-        render(
-            <MemoryRouter>
-                <ChooseChat/>
-            </MemoryRouter>
-        );
-        // Assert
-        const userChatBtn = screen.getByRole("button");
-
-    });
-    test("render hoose group chat btn");
+	test("render choose user chat btn", () => {
+		// Arange
+		render(
+			<MemoryRouter>
+				<ChooseChat />
+			</MemoryRouter>
+		);
+		// Assert
+		const userChatBtn = screen.getByRole("button", { name: /users/i });
+		expect(userChatBtn).toBeInTheDocument();
+	});
+	test("render choose group chat btn", () => {
+		// Arange
+		render(
+			<MemoryRouter>
+				<ChooseChat />
+			</MemoryRouter>
+		);
+		// Assert
+		const groupChatBtn = screen.getByRole("button", { name: /groups/i });
+		expect(groupChatBtn).toBeInTheDocument();
+	});
+	test("click choose user chat btn", () => {
+		// Arange
+		render(
+			<MemoryRouter>
+				<ChooseChat />
+			</MemoryRouter>
+		);
+		// Act
+		const userChatBtn = screen.getByRole("button", { name: /user/i });
+		userEvent.click(userChatBtn);
+		// Assert
+		expect(userChatBtn.getAttribute("class")).toMatch("active");
+	});
+	test("click choose group chat btn", () => {
+		// Arange
+		render(
+			<MemoryRouter>
+				<ChooseChat />
+			</MemoryRouter>
+		);
+		const groupChatBtn = screen.getByRole("button", { name: /groups/i });
+		userEvent.click(groupChatBtn);
+		// Assert
+		expect(groupChatBtn.getAttribute("class")).toMatch("active");
+	});
 });
