1ea309dd5d44bd64f66d3f37ca59863b1ddf0335
pkdc <pkdchoy@gmail.com>
2023-09-03 11:24:14 +0100

    test login wip

--- frontend/src/components/pages/LoginForm.test.js
+++ frontend/src/components/pages/LoginForm.test.js
@@ -1,69 +1,80 @@
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
-import { userEvent } from "@testing-library/user-event";
+import userEvent from "@testing-library/user-event";
 import LoginForm from "./LoginForm";
 
+describe("Login Form compo", () => {
+	test("renders Email Input", () => {
+		// Arrange
+		render(
+			<MemoryRouter>
+				<LoginForm />
+			</MemoryRouter>
+		);
+		// Act
+		// Assert
+		const emailInput = screen.getByLabelText("Email");
+		expect(emailInput).toBeInTheDocument();
+	});
 
-describe("Login Form compo", () => {  
-    test("renders Email Input", () => {
-        // Arrange
-        render(
-            <MemoryRouter>
-                <LoginForm />
-            </MemoryRouter>
-        );
-        // Act
-        // Assert
-        const emailInput = screen.getByLabelText("Email");
-        expect(emailInput).toBeInTheDocument();
-    });
-    
-    test("renders Password Input", () => {
-        // Arrange
-        render(
-            <MemoryRouter>
-                <LoginForm />
-            </MemoryRouter>
-        );
-        // Act
-        // Assert
-        const pwInput = screen.getByLabelText("Password", {exact: true});
-        expect(pwInput).toBeInTheDocument();
-    });
-    
-    test("render login btn", () => {
-        // Arrange
-        render(
-            <MemoryRouter>
-                <LoginForm />
-            </MemoryRouter>
-        );
-        // Act
-        //Assert
-        const loginBtn = screen.getByRole("button", { name: /login/i});
-        expect(loginBtn).toBeInTheDocument();
-    });
+	test("renders Password Input", () => {
+		// Arrange
+		render(
+			<MemoryRouter>
+				<LoginForm />
+			</MemoryRouter>
+		);
+		// Act
+		// Assert
+		const pwInput = screen.getByLabelText("Password", { exact: true });
+		expect(pwInput).toBeInTheDocument();
+	});
 
-    test("renders reg link", () => {
-        // Arrange
-        render(
-            <MemoryRouter>
-                <LoginForm />
-            </MemoryRouter>
-        );
-        // Act
-        // Assert
-        const regLinkEl = screen.getByRole("link", { name: /Register/i});
-        expect(regLinkEl).toBeInTheDocument();
-    });
+	test("render login btn", () => {
+		// Arrange
+		render(
+			<MemoryRouter>
+				<LoginForm />
+			</MemoryRouter>
+		);
+		// Act
+		//Assert
+		const loginBtn = screen.getByRole("button", { name: /login/i });
+		expect(loginBtn).toBeInTheDocument();
+	});
 
-    test("normal login success", () => {
-        render(
-            <MemoryRouter>
-                <LoginForm />
-            </MemoryRouter>
-        );
-        
+	test("renders reg link", () => {
+		// Arrange
+		render(
+			<MemoryRouter>
+				<LoginForm />
+			</MemoryRouter>
+		);
+		// Act
+		// Assert
+		const regLinkEl = screen.getByRole("link", { name: /Register/i });
+		expect(regLinkEl).toBeInTheDocument();
+	});
 
-    });
+	test("normal login success", async () => {
+		render(
+			<MemoryRouter>
+				<LoginForm />
+			</MemoryRouter>
+		);
+		const emailInput = screen.getByLabelText("Email");
+		userEvent.type(emailInput, "a@b");
+		const pwInput = screen.getByLabelText("Password", { exact: true });
+		userEvent.type(pwInput, "qwe");
+		const loginBtn = screen.getByRole("button", { name: /login/i });
+		userEvent.click(loginBtn);
+
+		const postBtn = await screen.findByRole(
+			"button",
+			{ name: /post/i },
+			{},
+			5000
+		);
+		expect(postBtn).toBeInTheDocument();
+	});
 });
