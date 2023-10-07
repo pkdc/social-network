b7d671e2724fa6ef4943d22e5b0c89b09695caee
pkdc <pkdchoy@gmail.com>
2023-10-07 04:49:01 +0100

    Loading Old Msgs (Old Msg >= 1 ) test wip

--- frontend/src/components/chat/Chatbox/Chatbox.test.js
+++ frontend/src/components/chat/Chatbox/Chatbox.test.js
@@ -19,9 +19,36 @@ describe("Old Chat Msg", () => {
 		expect(newChatMsg).toBeInTheDocument();
 	});
 
-	// test("Loading Old Msgs (Old Msg >= 1 )", async () => {
-	// 	// Assign
-	// 	// Act
-	// 	//Assert
-	// });
+	test("Loading Old Msgs (Old Msg >= 1 )", async () => {
+		// Assign
+		window.fetch = jest.fn();
+		window.fetch.mockResolvedValueOnce({
+			json: async () => {
+				{
+					data: [
+						{
+							createdat: "2023-03-16 00:00:00.0000000 +0100 +0100",
+							id: 1,
+							label: "",
+							message: "testOldMsg0",
+							onlineuserids: null,
+							sourceid: 1,
+							targetid: 2,
+						},
+						{
+							createdat: "2023-03-16 00:01:00.0000000 +0100 +0100",
+							id: 2,
+							label: "",
+							message: "testOldMsg1",
+							onlineuserids: null,
+							sourceid: 2,
+							targetid: 1,
+						},
+					];
+				}
+			},
+		});
+		// Act
+		//Assert
+	});
 });
